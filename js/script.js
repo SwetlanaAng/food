2448931938faedae8ceda8d92786d2359af4d3bb
Sveta Angelyik <floss2@yandex.ru>
2024-05-31 09:28:57 +0300

    script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
 'use strict'
-
-//const { response } = require("express");
+import calc from './modules/calc';
+        import cards from './modules/cards';
+        import forms from './modules/forms';
+        import modal from './modules/modal';
+        import slider from './modules/slider';
+        import tabs from './modules/tabs';
+        import timer from './modules/timer';
+        import { openModal } from './modules/modal';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const calc = require('./modules/calc'),
-        cards = require('./modules/cards'),
-        forms = require('./modules/forms'),
-        modal = require('./modules/modal'),
-        slider = require('./modules/slider'),
-        tabs = require('./modules/tabs'),
-        timer = require('./modules/timer');
+    const showModalSoon = setInterval(()=>openModal('.modal', showModalSoon), 500000000);    
     calc();
     cards();
-    forms();
-    modal();
-    slider();
-    tabs();
-    timer();
+    forms('form');
+    modal('.btn', '.modal', showModalSoon);
+    slider({
+        prevSelector:'.offer__slider-prev',
+        sliderSelector:'.offer__slider',
+        nextSelector: '.offer__slider-next',
+        currentSelector: '#current',
+        totalSelector: '#total',
+        slidesSelector: '.offer__slide',
+        slidesFieldSelector: '.offer__slider-inner',
+        slidesWrapperSelector:'.offer__slider-wrapper' 
+    });
+    tabs('.tabcontent', '.tabheader', '.tabheader__item', 'tabheader__item_active');
+    timer('2024-06-09');
     
 })
